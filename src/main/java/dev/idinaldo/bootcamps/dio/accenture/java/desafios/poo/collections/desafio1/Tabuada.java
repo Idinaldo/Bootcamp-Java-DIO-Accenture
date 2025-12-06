79353174e2808cbb6224e9be5e1444eb6588b618
Idinaldo <idinaldooliveira220@gmail.com>
2025-12-06 02:40:18 -0300

    feat: add desafio1 directory, first challenge description and solution.

---
+++ src/main/java/dev/idinaldo/bootcamps/dio/accenture/java/desafios/poo/collections/desafio1/Tabuada.java
@@ -0,0 +1,26 @@
+package dev.idinaldo.bootcamps.dio.accenture.java.desafios.poo.collections.desafio1;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Tabuada {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        List<String> numbersStr = List.of(scanner.nextLine().split(","));
+
+        tabuadaAdicao(numbersStr);
+
+    }
+
+    private static void tabuadaAdicao(List<String> numbers) {
+        for (String number : numbers) {
+            int temp = Integer.valueOf(number);
+            for (int i = 1; i <= 10; i++) {
+                System.out.printf("%d + %d = %d\n", temp, i, temp + i);
+            }
+        }
+    }
+}
+
